40a935786517f6e29ead5b2adec8bc05d6a94087
AshokAndani <andani.ashok007@gmail.com>
2023-04-29 11:00:51 +0530

    [ashok]Fix: Added Maths.Add functionality

--- Program.cs
+++ Program.cs
@@ -5,6 +5,22 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            Maths maths = new Maths();
+            Console.WriteLine("Enter 1 for Addition");
+            int input = int.Parse(Console.ReadLine());
+            switch (input)
+            {
+                case 1:
+                    Console.WriteLine("Enter 1 number: ");
+                    int a = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter 2nd Number:");
+                    int b = int.Parse(Console.ReadLine());
+                    int result = maths.Add(a, b);
+                    Console.WriteLine($"Addition of {a} and {b} is: {result}");
+                    break;
+                default: Console.WriteLine("Invalid Entry"); break;
+            }
         }
     }
 }
